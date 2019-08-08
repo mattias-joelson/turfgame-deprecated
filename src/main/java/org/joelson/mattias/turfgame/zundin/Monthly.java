7b37a65c16f8c8ff0de2a8472884ceb83cab2c6a
Mattias Joëlson <mattias@joelson.org>
2019-08-08 12:29:13 +0200

    Import cleanup.

--- src/main/java/org/joelson/mattias/turfgame/zundin/Monthly.java
+++ src/main/java/org/joelson/mattias/turfgame/zundin/Monthly.java
@@ -11,9 +11,7 @@ import org.joelson.mattias.turfgame.util.URLReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Monthly {
 
