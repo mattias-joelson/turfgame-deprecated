7edc98e922cbf79cec6b851de8d64927132fb729
Mattias Joëlson <mattias.joelson@looklet.com>
2019-09-11 11:34:01 +0200

    Divide flips into Folders.

--- src/test/java/org/joelson/mattias/turfgame/zundin/Flipp08MissionTest.java
+++ src/test/java/org/joelson/mattias/turfgame/zundin/Flipp08MissionTest.java
@@ -1,28 +1,31 @@
 package org.joelson.mattias.turfgame.zundin;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.joelson.mattias.turfgame.apiv4.Zone;
 import org.joelson.mattias.turfgame.apiv4.ZonesTest;
 import org.joelson.mattias.turfgame.util.JSONArray;
+import org.joelson.mattias.turfgame.util.JSONNumber;
 import org.joelson.mattias.turfgame.util.JSONObject;
 import org.joelson.mattias.turfgame.util.JSONParser;
 import org.joelson.mattias.turfgame.util.JSONString;
 import org.joelson.mattias.turfgame.util.JSONValue;
-import org.joelson.mattias.turfgame.util.URLReader;
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class Flipp08MissionTest {
     
@@ -35,14 +38,12 @@ public class Flipp08MissionTest {
         }
         
         File jsonFile = new File(Flipp08MissionTest.class.getResource("/flipp08zones.json").getFile());
-        //StringBuffer jsonBuffer = new StringBuffer();
         List<JSONObject> flips = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(jsonFile), "UTF8"))) {
             String line;
             while ((line = in.readLine()) != null) {
-                System.out.println(line);
+                System.out.println("Parsing " + line);
                 JSONObject flip = (JSONObject) new JSONParser().parse(line);
-                System.out.println(flip);
                 flips.add(flip);
             }
         }
@@ -51,30 +52,39 @@ public class Flipp08MissionTest {
         out.println("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>");
         out.println("<kml xmlns=\"http://www.opengis.net/kml/2.2\" xmlns:atom=\"http://www.w3.org/2005/Atom\" xmlns:gx=\"http://www.google.com/kml/ext/2.2\" xmlns:xal=\"urn:oasis:names:tc:ciq:xsdschema:xAL:2.0\">");
         out.println("  <Document>");
-        out.println("    <Folder>");
-        out.println("      <name>Flip08</name>");
 
         for (JSONObject flip : flips) {
-            String name = ((JSONString) flip.getValue("name")).stringValue();
-            System.out.println(name);
+            String flipName = ((JSONString) flip.getValue("name")).stringValue();
+            System.out.println("Processing " + flipName);
+            int flipZoneCount = ((JSONNumber) flip.getValue("zoneCount")).intValue();
+            out.println("    <Folder>");
+            out.println("      <name>" + flipName + "</name>");
             JSONArray array = (JSONArray) flip.getValue("zones");
             int count = 0;
+            Set<String> uniqueZoneNames = new HashSet<>();
             for (JSONValue value : array.getElements()) {
                 String zoneName = ((JSONString) value).stringValue();
-                System.out.println(zoneName);
+                uniqueZoneNames.add(zoneName);
+                Zone zone = zoneMap.get(zoneName);
+                if (zone == null) {
+                    System.err.println("Zone '" + zoneName + "' not found!");
+                    assertTrue(zone != null);
+                }
+                System.out.println("  " + zoneName);
                 out.println("        <Placemark>");
                 out.println("          <name>" + zoneName + "</name>");
-                out.println("          <description>" + name + " - " + zoneName + "</description>");
-                Zone zone = zoneMap.get(zoneName);
+                out.println("          <description>" + flipName + " - " + zoneName + "</description>");
                 out.println("          <Point>");
                 out.println("            <coordinates>" + zone.getLongitude() + ',' + zone.getLatitude() + "</coordinates>");
                 out.println("          </Point>");
                 out.println("        </Placemark>");
                 count += 1;
             }
-            System.out.println("  zones: " + count);
+            out.println("    </Folder>");
+            System.out.println("  zones: " + count + ", unique: " + uniqueZoneNames.size() + ", same: " + (count == uniqueZoneNames.size()));
+            assertEquals(count, uniqueZoneNames.size());
+            assertEquals(flipZoneCount, count);
         }
-        out.println("    </Folder>");
         out.println("  </Document>");
         out.println("</kml>");
         out.close();
