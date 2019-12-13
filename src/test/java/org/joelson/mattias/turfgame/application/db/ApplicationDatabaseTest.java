fb47396db4df69fd799d64cb9bfc9725a29e16d8
Mattias Joëlson <mattias.joelson@looklet.com>
2019-12-13 17:14:13 +0100

    Not working but...

---
+++ src/test/java/org/joelson/mattias/turfgame/application/db/ApplicationDatabaseTest.java
@@ -0,0 +1,27 @@
+package org.joelson.mattias.turfgame.application.db;
+
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertNotNull;
+
+public class ApplicationDatabaseTest {
+    
+    public static final String CONNECTION_URL_H2 = "jdbc:h2:./output/turfgame_test_h2";
+    public static final String CONNECTION_URL_DERBY = "jdbc:derby:output/turfgame_test_derby;create=true";
+    
+    @Test
+    public void testDerby() throws SQLException, ClassNotFoundException {
+        ApplicationDatabase database = new ApplicationDatabase(ApplicationDatabaseTest.CONNECTION_URL_DERBY);
+        assertNotNull(database);
+        System.out.println(Class.forName("org.apache.derby.jdbc.EmbeddedDriver"));
+    }
+    
+    @Test
+    public void testH2() throws SQLException, ClassNotFoundException {
+        ApplicationDatabase database = new ApplicationDatabase(ApplicationDatabaseTest.CONNECTION_URL_H2);
+        assertNotNull(database);
+        System.out.println(Class.forName("org.h2.Driver"));
+    }
+}
