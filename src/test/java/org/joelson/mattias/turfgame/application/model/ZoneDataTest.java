f8295d6af6a60420cc243883dbab3930d2c32e13
Mattias Joëlson <mattias.joelson@looklet.com>
2019-12-16 16:29:22 +0100

    Time zones are tricky...

--- src/test/java/org/joelson/mattias/turfgame/application/model/ZoneDataTest.java
+++ src/test/java/org/joelson/mattias/turfgame/application/model/ZoneDataTest.java
@@ -8,14 +8,32 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ZoneDataTest {
     
     private static final String PERSISTANCE_DERBY = "turfgame-test-derby";
     private static final String PERSISTANCE_H2 = "turfgame-test-h2";
+    
+    @Test
+    public void instantTest() {
+        Instant i1 = Instant.parse("2019-11-30T12:12:00.00Z");
+        Instant i2 = Instant.parse("2019-11-30T11:12:00.00Z");
+        assertTrue(i2.isBefore(i1));
+        Instant i3 = toInstant("2019-11-30T12:12:00");
+        assertTrue(i3.equals(i2));
+        assertTrue(i1.isAfter(i3));
+        Instant i4 = toInstant("2019-11-30T12:12:00+01:00");
+        assertTrue(i4.equals(i3));
+    }
     
     @Test
     public void zoneDataDerbyTest() throws IOException {
@@ -28,21 +46,29 @@ public class ZoneDataTest {
     }
     
     private static void testData(ZoneData zoneData) throws IOException {
-        zoneData.updateZones(Instant.parse("2019-11-30T12:12:00.00Z"), getZones("zones-all.2019-11-30.json"));
+        zoneData.updateZones(toInstant("2019-11-30T12:12:00.00+01:00"), getZones("zones-all.2019-11-30.json"));
         
         assertEquals(71, zoneData.getZone("FolkeFilbyter").getId());
         assertEquals(63920, zoneData.getZones().size());
         assertEquals(63920, zoneData.getZoneDataHistory().size());
         assertEquals(63920, zoneData.getZonePointsHistory().size());
         
-        zoneData.updateZones(Instant.parse("2019-12-01T22:42:00.00Z"), getZones("zones-all.2019-12-01.json"));
+        zoneData.updateZones(toInstant("2019-12-01T22:42:00.00+01:00"), getZones("zones-all.2019-12-01.json"));
     
         assertEquals(63927, zoneData.getZones().size());
         assertEquals(63927, zoneData.getZoneDataHistory().size());
         assertEquals(77344, zoneData.getZonePointsHistory().size());
     }
     
-    public static List<Zone> getZones(String filename) throws IOException {
+    private static List<Zone> getZones(String filename) throws IOException {
         return URLReaderTest.readProperties(String.format("/%s", filename), Zones::fromHTML);
     }
+    
+    private static Instant toInstant(String s) {
+        TemporalAccessor temporalAccessor = DateTimeFormatter.ISO_DATE_TIME.parse(s);
+        LocalDateTime localDateTime = LocalDateTime.from(temporalAccessor);
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime, ZoneId.systemDefault());
+        return Instant.from(zonedDateTime);
+    }
+    
 }
