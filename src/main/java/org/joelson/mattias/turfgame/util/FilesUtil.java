e7f002ebda119d79205217647504316a82bb6142
Mattias Joëlson <mattias.joelson@looklet.com>
2020-01-02 19:50:21 +0100

    Create parent directory if does not already exists.

--- src/main/java/org/joelson/mattias/turfgame/util/FilesUtil.java
+++ src/main/java/org/joelson/mattias/turfgame/util/FilesUtil.java
@@ -19,7 +19,6 @@ public final class FilesUtil {
             StandardOpenOption.CREATE,
             StandardOpenOption.TRUNCATE_EXISTING
     };
-    private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
     
     private FilesUtil() throws InstantiationException {
         throw new InstantiationException("Should not be instantiated!");
@@ -32,11 +31,18 @@ public final class FilesUtil {
         return Paths.get(DEFAULT_OUTPUT_DIRECTORY, filenameParts);
     }
     
+    private static Path createParentDirectory(Path path) throws IOException {
+        if (!Files.exists(path.getParent())) {
+            Files.createDirectory(path.getParent());
+        }
+        return path;
+    }
+
     public static OutputStream newDefaultOutputStream(String... filenameParts) throws IOException {
-        return Files.newOutputStream(filenamePath(filenameParts), DEFAULT_OPEN_OPTIONS);
+        return Files.newOutputStream(createParentDirectory(filenamePath(filenameParts)), DEFAULT_OPEN_OPTIONS);
     }
     
     public static BufferedWriter newDefaultWriter(String... filenameParts) throws IOException {
-        return Files.newBufferedWriter(filenamePath(filenameParts), DEFAULT_CHARSET, DEFAULT_OPEN_OPTIONS);
+        return Files.newBufferedWriter(createParentDirectory(filenamePath(filenameParts)), DEFAULT_OPEN_OPTIONS);
     }
 }
