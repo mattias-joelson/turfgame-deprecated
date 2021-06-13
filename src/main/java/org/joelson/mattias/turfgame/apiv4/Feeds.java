42c4dfaca98df149d18b28acebbc158188bf0f7b
Mattias Joëlson <mattias.joelson@looklet.com>
2021-06-13 07:53:14 +0200

    Keep track of time of last read.

--- src/main/java/org/joelson/mattias/turfgame/apiv4/Feeds.java
+++ src/main/java/org/joelson/mattias/turfgame/apiv4/Feeds.java
@@ -1,59 +1,113 @@
 package org.joelson.mattias.turfgame.apiv4;
 
+import org.joelson.mattias.turfgame.util.JSONArray;
+import org.joelson.mattias.turfgame.util.JSONObject;
+import org.joelson.mattias.turfgame.util.JSONString;
+import org.joelson.mattias.turfgame.util.JSONValue;
+import org.joelson.mattias.turfgame.util.TimeUtil;
 import org.joelson.mattias.turfgame.util.URLReader;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.text.ParseException;
 import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 public class Feeds {
 
     private static final String FEEDS_REQUEST = "https://api.turfgame.com/v4/feeds"; //NON-NLS
 
-    private static String getFeedsJSON() throws IOException {
-        return URLReader.getRequest(FEEDS_REQUEST);
-    }
-
-    private static String getFeedsJSON(String feed) throws IOException {
-        return URLReader.getRequest(FEEDS_REQUEST + '/' + feed);
-    }
-
-    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
+    private static DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
 
     public static void main(String[] args) {
+        new Thread(() -> handleFeed("takeover", "feeds_takeover_%s.json")).start();
+        new Thread(() -> handleFeed("medal+chat", "feeds_medal_chat_%s.json")).start();
+        new Thread(() -> handleFeed("zone", "feeds_zone_%s.json")).start();
+    }
 
+    private static void handleFeed(String feed, String filenamePattern) {
+        Instant lastEntry = null;
         for (;;) {
-            getFeed("takeover", "feeds_takeover_%s.json");
-            getFeed("medal+chat", "feeds_medal_chat_%s.json");
-            getFeed("zone", "feeds_zone_%s.json");
-            Instant until = Instant.now().plusSeconds(5 * 60);
-            while (Instant.now().isBefore(until)) {
-                System.out.println("Sleeping at " + Instant.now());
-                try {
-                    Thread.sleep(10000);
-                } catch (InterruptedException e) {
-                    // ignore
-                }
+            lastEntry = getFeed(feed, filenamePattern, lastEntry);
+            waitUntilNext();
+        }
+    }
+
+    private static Instant getFeed(String feed, String filenamePattern, Instant since) {
+        try {
+            String json = getFeedsJSON(feed, since);
+            if (json.equals("[]")) {
+                log("No data for " + feed + " since " + since);
+                return since;
+            }
+            Instant lastEntryTime = getLastEntryTime(json);
+            if (lastEntryTime == null) {
+                log("JSON: " + json);
+                log("lastEntryTime is null");
+            }
+            Path file = getFilePath(filenamePattern, lastEntryTime);
+            Files.writeString(file, json, StandardCharsets.UTF_8);
+            log("Downloaded " + file + " at " + Instant.now());
+            return Instant.from(lastEntryTime).minusSeconds(1);
+        } catch (Throwable e) {
+            log(Instant.now() + ": " + e);
+            return null;
+        }
+    }
+    private static String getFeedsJSON(String feed, Instant since) throws IOException {
+        String afterDate = "";
+        if (since != null) {
+            afterDate = "?afterDate=" + TimeUtil.turfAPITimestampFormatter(since);
+        }
+        return URLReader.getRequest(FEEDS_REQUEST + '/' + feed + afterDate);
+    }
+
+    private static Instant getLastEntryTime(String json) throws ParseException {
+        JSONArray array = JSONArray.parseArray(json);
+        Instant latest = null;
+        for (JSONValue value : array.getElements()) {
+            JSONObject object = (JSONObject) value;
+            String timeStamp = ((JSONString) object.getValue("time")).stringValue();
+            Instant instant = TimeUtil.turfAPITimestampToInstant(timeStamp);
+            if (latest == null || instant.isAfter(latest)) {
+                latest = instant;
+            }
+        }
+        return latest;
+    }
+
+    private static Path getFilePath(String filenamePattern, Instant lastEntryTime) throws IOException {
+        String timeString = toTimeString(lastEntryTime);
+        String name = String.format(filenamePattern, timeString);
+        Path filePath = Path.of(".", name);
+        if (Files.exists(filePath)) {
+            filePath = Files.createTempFile(Path.of("."), name.substring(0, name.indexOf(".json") + 1), ".json");
+        }
+        return filePath;
+    }
+
+    private static String toTimeString(Instant instant) {
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneOffset.UTC);
+        return DATE_TIME_FORMATTER.format(localDateTime);
+    }
+
+    private static void waitUntilNext() {
+        Instant until = Instant.now().plusSeconds(5 * 60);
+        log("Sleeping until " + until);
+        while (Instant.now().isBefore(until)) {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                // ignore
             }
         }
     }
 
-    private static void getFeed(String feed, String filename) {
-        try {
-            Instant instant = Instant.now();
-            LocalDateTime ldt = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
-            String time = formatter.format(ldt);
-            String name = String.format(filename, time);
-            Path file = Path.of(".", name);
-            Files.writeString(file, getFeedsJSON(feed), StandardCharsets.UTF_8);
-            System.out.println("Downloaded " + file + " at " + Instant.now());
-        } catch (IOException e) {
-            System.out.println(Instant.now() + ": " + e);
-        }
+    private static void log(String msg) {
+        System.out.println(String.format("[%s] %s", Thread.currentThread().getName(), msg));
     }
 }
