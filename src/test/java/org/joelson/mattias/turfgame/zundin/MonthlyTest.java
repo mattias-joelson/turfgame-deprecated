cd7f21158173276201197ee4bf22184205815c07
Mattias Joëlson <mattias@joelson.org>
2022-04-03 10:56:29 +0200

    Statistics round 141 - March 2022

--- src/test/java/org/joelson/mattias/turfgame/zundin/MonthlyTest.java
+++ src/test/java/org/joelson/mattias/turfgame/zundin/MonthlyTest.java
@@ -66,7 +66,7 @@ public class MonthlyTest {
         assertNotNull(monthly);
         assertEquals(OBEROFF, monthly.getUserName());
         assertEquals(ROUND, monthly.getRound());
-        assertEquals(122, (monthly.getZones()).size());
+        assertEquals(173, (monthly.getZones()).size());
     }
 
     @Test
@@ -77,7 +77,7 @@ public class MonthlyTest {
     }
 
     public static Monthly getMonthly() throws Exception {
-        return readProperties("monthly_0beroff_round140.html");
+        return readProperties("monthly_0beroff_round141.html");
     }
 
     private static Monthly readProperties(String resource) throws Exception {
