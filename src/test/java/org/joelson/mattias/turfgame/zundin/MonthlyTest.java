caafe7adf9d122749e6f0baadb4760cb89a3a8e9
Mattias Joëlson <mattias@joelson.org>
2023-11-07 07:15:29 +0100

    Statistics round 160 - October 2023

--- src/test/java/org/joelson/mattias/turfgame/zundin/MonthlyTest.java
+++ src/test/java/org/joelson/mattias/turfgame/zundin/MonthlyTest.java
@@ -78,7 +78,7 @@ public class MonthlyTest {
     }
 
     public static Monthly getMonthly() throws Exception {
-        return readProperties("monthly_0beroff_round159.html");
+        return readProperties("monthly_0beroff_round160.html");
     }
 
     private static Monthly readProperties(String resource) throws Exception {
