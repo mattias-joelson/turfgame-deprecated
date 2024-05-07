cf6cd25e09bf4d54114c32aea2ae048716be8caa
Mattias Joëlson <mattias@joelson.org>
2024-05-07 07:23:15 +0200

    Statistics round 166 - April 2024

--- src/test/java/org/joelson/mattias/turfgame/zundin/MonthlyTest.java
+++ src/test/java/org/joelson/mattias/turfgame/zundin/MonthlyTest.java
@@ -78,7 +78,7 @@ public class MonthlyTest {
     }
 
     public static Monthly getMonthly() throws Exception {
-        return readProperties("monthly_0beroff_round165.html");
+        return readProperties("monthly_0beroff_round166.fairy.html");
     }
 
     private static Monthly readProperties(String resource) throws Exception {
