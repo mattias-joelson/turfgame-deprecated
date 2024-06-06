80e0207c8df45a2427e283b353f43114d0215872
Mattias Joëlson <mattias@joelson.org>
2024-06-06 13:07:00 +0200

    Start moving org.joelson.mattias.turfgame.apiv5

--- modules/resources/src/main/java/org/joelson/turf/turfgame/apiv5/Region.java
+++ modules/resources/src/main/java/org/joelson/turf/turfgame/apiv5/Region.java
@@ -1,31 +1,41 @@
-package org.joelson.mattias.turfgame.apiv5;
+package org.joelson.turf.turfgame.apiv5;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.joelson.mattias.turfgame.util.StringUtil;
+import org.joelson.turf.util.StringUtil;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public class Region {
 
+    private final int id;
+    private final String name;
     private final String country;
     private final Area area;
-    private final String name;
-    private final int id;
+    private final User regionLord;
 
     @JsonCreator
     public Region(
-            @Nullable @JsonProperty("country") String country,
-            @Nonnull @JsonProperty("area") Area area,
+            @Nonnull @JsonProperty("id") int id,
             @Nonnull @JsonProperty("name") String name,
-            @JsonProperty("id") int id,
-            @JsonProperty("regionLord") User regionLord
+            @Nullable @JsonProperty("country") String country,
+            @Nullable @JsonProperty("area") Area area,
+            @Nullable @JsonProperty("regionLord") User regionLord
     ) {
         this.country = StringUtil.requireNullOrNonEmpty(country);
         this.area = area;
         this.name = StringUtil.requireNotNullAndNotTrimmedEmpty(name);
         this.id = id;
+        this.regionLord = regionLord;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public String getCountry() {
@@ -36,11 +46,7 @@ public class Region {
         return area;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public int getId() {
-        return id;
+    public User getRegionLord() {
+        return regionLord;
     }
 }
