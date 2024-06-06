dd21cdd5b2e4b595ea535454fce4ee3a0a0639ff
Mattias Joëlson <mattias@joelson.org>
2024-06-06 21:41:33 +0200

    Reoriganized feed downloads.

--- modules/utilities/src/main/java/org/joelson/turf/util/TimeUtil.java
+++ modules/utilities/src/main/java/org/joelson/turf/util/TimeUtil.java
@@ -1,4 +1,4 @@
-package org.joelson.mattias.turfgame.util;
+package org.joelson.turf.util;
 
 import java.io.IOException;
 import java.time.Instant;
@@ -10,29 +10,34 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
 
 public final class TimeUtil {
-    
+
     private static final int TIMESTAMP_END_INDEX = 19;
-    private static DateTimeFormatter TURF_API_FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE_TIME.withZone(ZoneId.from(ZoneOffset.UTC));
-    private static final DateTimeFormatter ZUNDIN_GMT_DATETIME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"); //NON-NLS
+    private static final DateTimeFormatter TURF_API_FORMATTER =
+            DateTimeFormatter.ISO_LOCAL_DATE_TIME.withZone(ZoneId.from(ZoneOffset.UTC));
+    private static final DateTimeFormatter ZUNDIN_GMT_DATETIME_FORMAT =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"); //NON-NLS
 
     private TimeUtil() throws InstantiationException {
         throw new InstantiationException("Should not be instantiated!");
     }
-    
+
     /**
      * Converts a Turf API timestamp to a Java Instant.
+     *
      * @param timestamp on format "2016-02-10T20:47:08+0000", "+0000" can be omitted
      * @return instant of timestamp
      */
     public static Instant turfTimestampToInstant(String timestamp) {
-        TemporalAccessor temporalAccessor = DateTimeFormatter.ISO_DATE_TIME.parse(timestamp.substring(0, TIMESTAMP_END_INDEX));
+        TemporalAccessor temporalAccessor =
+                DateTimeFormatter.ISO_DATE_TIME.parse(timestamp.substring(0, TIMESTAMP_END_INDEX));
         LocalDateTime localDateTime = LocalDateTime.from(temporalAccessor);
         ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime, ZoneId.systemDefault());
         return Instant.from(zonedDateTime);
     }
 
     public static Instant turfAPITimestampToInstant(String timestamp) {
-        TemporalAccessor temporalAccessor = DateTimeFormatter.ISO_DATE_TIME.parse(timestamp.substring(0, TIMESTAMP_END_INDEX));
+        TemporalAccessor temporalAccessor =
+                DateTimeFormatter.ISO_DATE_TIME.parse(timestamp.substring(0, TIMESTAMP_END_INDEX));
         LocalDateTime localDateTime = LocalDateTime.from(temporalAccessor);
         ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime, ZoneOffset.UTC);
         return Instant.from(zonedDateTime);
@@ -44,6 +49,7 @@ public final class TimeUtil {
 
     /**
      * Converts a Zundin GMT timestamp to a Java Instant.
+     *
      * @param timestamp on format "2016-02-10 20:47:58#
      * @return instant of timestamp
      */
