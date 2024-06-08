1c35dcae0163eafb3c3f6fb34a59aed9ad48f1c8
Mattias Joëlson <mattias@joelson.org>
2024-06-08 12:33:52 +0200

    Monthly tests (missing v5 zones)

--- modules/utilities/src/main/java/org/joelson/turf/util/KMLWriter.java
+++ modules/utilities/src/main/java/org/joelson/turf/util/KMLWriter.java
@@ -1,14 +1,14 @@
-package org.joelson.mattias.turfgame.util;
+package org.joelson.turf.util;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Writer;
 
 public class KMLWriter extends Writer {
-    
+
     private final PrintWriter out;
     private boolean hasFolder;
-    
+
     public KMLWriter(String... filenameParts) throws IOException {
         out = new PrintWriter(FilesUtil.newDefaultWriter(filenameParts));
         hasFolder = false;
@@ -17,7 +17,9 @@ public class KMLWriter extends Writer {
 
     private void writeHeader() {
         out.println("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>");
-        out.println("<kml xmlns=\"http://www.opengis.net/kml/2.2\" xmlns:atom=\"http://www.w3.org/2005/Atom\" xmlns:gx=\"http://www.google.com/kml/ext/2.2\" xmlns:xal=\"urn:oasis:names:tc:ciq:xsdschema:xAL:2.0\">");
+        out.println("<kml xmlns=\"http://www.opengis.net/kml/2.2\" xmlns:atom=\"http://www.w3.org/2005/Atom\" "
+                + "xmlns:gx=\"http://www.google.com/kml/ext/2.2\" "
+                + "xmlns:xal=\"urn:oasis:names:tc:ciq:xsdschema:xAL:2.0\">");
         out.println("  <Document>");
     }
 
@@ -25,7 +27,7 @@ public class KMLWriter extends Writer {
     public void write(char[] cbuf, int off, int len) throws IOException {
         out.write(cbuf, off, len);
     }
-    
+
     public void writeFolder(String name) {
         if (hasFolder) {
             out.println("    </Folder>");
@@ -34,7 +36,7 @@ public class KMLWriter extends Writer {
         out.println("      <name>" + name + "</name>");
         hasFolder = true;
     }
-    
+
     public void writePlacemark(String name, String description, double longitude, double latitude) {
         out.println("        <Placemark>");
         out.println("          <name>" + name + "</name>");
