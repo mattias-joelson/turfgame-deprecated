55342d24ee81e22cf99e377f6716c48610a2ec88
Mattias Joëlson <mattias@joelson.org>
2024-06-08 16:50:20 +0200

    persistence-utilities

--- modules/persistence-utilities/src/main/java/org/joelson/turf/util/persistence/EntityRegistry.java
+++ modules/persistence-utilities/src/main/java/org/joelson/turf/util/persistence/EntityRegistry.java
@@ -1,4 +1,4 @@
-package org.joelson.mattias.turfgame.util.db;
+package org.joelson.turf.util.persistence;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
@@ -7,46 +7,47 @@ import java.lang.reflect.ParameterizedType;
 import java.util.stream.Stream;
 
 public class EntityRegistry<T> {
-    
+
     private final EntityManager entityManager;
     private final Class<T> persistentClass;
-    
+
     @SuppressWarnings("unchecked")
     public EntityRegistry(EntityManager entityManager) {
         this.entityManager = entityManager;
-        persistentClass = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+        persistentClass =
+                (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
     }
-    
+
     protected TypedQuery<T> createQuery(String s) {
         return entityManager.createQuery(s, persistentClass);
     }
-    
+
     public T find(int id) {
         return entityManager.find(persistentClass, id);
     }
-    
+
     public T find(Object key) {
         return entityManager.find(persistentClass, key);
     }
-    
+
     public T findAnyOrNull(String field, Object value) {
-        return findAll(field, value)
-                .findAny()
-                .orElse(null);
+        return findAll(field, value).findAny().orElse(null);
     }
-    
+
     public Stream<T> findAll() {
-        TypedQuery<T>  query = entityManager.createQuery(String.format("SELECT e FROM %s e", persistentClass.getName()), persistentClass); //NON-NLS
-        return query.getResultStream();
-    }
-    
-    public Stream<T> findAll(String field, Object value) {
-        TypedQuery<T> query = entityManager.createQuery(String.format("SELECT e FROM %s e WHERE e.%s=:value", persistentClass.getName(), field), //NON-NLS
+        TypedQuery<T> query = entityManager.createQuery(String.format("SELECT e FROM %s e", persistentClass.getName()),
                 persistentClass);
-        query.setParameter("value", value); //NON-NLS
         return query.getResultStream();
     }
-    
+
+    public Stream<T> findAll(String field, Object value) {
+        TypedQuery<T> query = entityManager.createQuery(
+                String.format("SELECT e FROM %s e WHERE e.%s=:value", persistentClass.getName(), field),
+                persistentClass);
+        query.setParameter("value", value);
+        return query.getResultStream();
+    }
+
     public void persist(T t) {
         entityManager.persist(t);
     }
