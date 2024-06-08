df266aa7fe7261b39665f954b1efe895fef59f95
Mattias Joëlson <mattias@joelson.org>
2024-06-08 12:33:52 +0200

    Monthly tests (missing v5 zones)

--- modules/resources/src/test/java/org/joelson/turf/util/URLReaderTest.java
+++ modules/resources/src/test/java/org/joelson/turf/util/URLReaderTest.java
@@ -7,10 +7,6 @@ import java.util.function.Function;
 
 public final class URLReaderTest {
 
-    private URLReaderTest() throws InstantiationException {
-        throw new InstantiationException("Should not be instantiated!");
-    }
-
     public static <R> R readProperties(String resource, Function<String, R> function) throws IOException {
         File file = new File(URLReaderTest.class.getResource("/" + resource).getFile());
         try (FileInputStream input = new FileInputStream(file)) {
