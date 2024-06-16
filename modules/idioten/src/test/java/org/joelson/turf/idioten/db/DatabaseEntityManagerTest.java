917f57f0c93a5a65019c788f0d50e7680ddd3686
Mattias Joëlson <mattias@joelson.org>
2024-06-16 16:35:03 +0200

    Spelling and test more methods.

--- modules/idioten/src/test/java/org/joelson/turf/idioten/db/DatabaseEntityManagerTest.java
+++ modules/idioten/src/test/java/org/joelson/turf/idioten/db/DatabaseEntityManagerTest.java
@@ -10,27 +10,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DatabaseEntityManagerTest {
 
-    public static final String PERSISTANCE_H2 = "turfgame-idioten-test-h2";
+    public static final String PERSISTENCE_H2 = "turfgame-idioten-test-h2";
 
     @Test
     public void testUpdateUsers() {
-        DatabaseEntityManager entityManager = new DatabaseEntityManager(PERSISTANCE_H2);
+        DatabaseEntityManager entityManager = new DatabaseEntityManager(PERSISTENCE_H2);
         entityManager.updateUsers(List.of(new UserData(1, "one")), List.of());
         assertEquals(1, entityManager.getUsers().size());
         entityManager.updateUsers(List.of(new UserData(2, "two")), List.of(new UserData(1, "onesize")));
         assertEquals(2, entityManager.getUsers().size());
         assertEquals("onesize", entityManager.getUser(1).getName());
+        assertEquals(2, entityManager.getUser("two").getId());
         entityManager.close();
     }
 
     @Test
     public void testUpdateZones() {
-        DatabaseEntityManager entityManager = new DatabaseEntityManager(PERSISTANCE_H2);
+        DatabaseEntityManager entityManager = new DatabaseEntityManager(PERSISTENCE_H2);
         entityManager.updateZones(List.of(new ZoneData(1, "Zonen")), List.of());
         assertEquals(1, entityManager.getZones().size());
         entityManager.updateZones(List.of(new ZoneData(2, "NyZon")), List.of(new ZoneData(1, "Zonett")));
         assertEquals(2, entityManager.getZones().size());
         assertEquals("Zonett", entityManager.getZone(1).getName());
+        assertEquals(2, entityManager.getZone("NyZon").getId());
         entityManager.close();
     }
 }
